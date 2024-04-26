bb657ecdd65fa16332d5387919be1a0d5bf8f102
mkudinov <m.kudinov@icloud.com>
2024-04-26 21:59:30 +0300

    improve run script

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-/home/mkudinov/intelFPGA_lite/21.1/quartus/bin/quartus_sh --flow compile game_top &&
-/home/mkudinov/intelFPGA_lite/21.1/quartus/bin/quartus_pgm -c USB-Blaster --mode=jtag -o "p;output_files/game_top.sof"
+~/intelFPGA_lite/21.1/quartus/bin/quartus_sh --flow compile quartus/pong_fpga &&
+~/intelFPGA_lite/21.1/quartus/bin/quartus_pgm -c USB-Blaster --mode=jtag -o "p;quartus/output_files/pong_fpga.sof"
