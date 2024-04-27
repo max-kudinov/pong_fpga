b9882ddd553c32ca1471e22fef1bcc6627a0d756
mkudinov <m.kudinov@icloud.com>
2024-04-27 22:35:19 +0300

    vga refactor and bug fix

--- vga.sv
+++ vga.sv
@@ -8,22 +8,22 @@ module vga
     parameter H_FRONT_PORCH = 16,
     parameter H_BACK_PORCH  = 48,
     parameter H_BORDER      = 0,
-    parameter X_POS_W       = $clog2(H_RES),
+    parameter X_POS_W       = 10,
 
     parameter V_RES         = 480,
     parameter VSYNC_PULSE   = 2,
     parameter V_FRONT_PORCH = 10,
     parameter V_BACK_PORCH  = 33,
     parameter V_BORDER      = 0,
-    parameter Y_POS_W       = $clog2(V_RES)
+    parameter Y_POS_W       = 9
 )
 (
     input  logic                 clk_i,
     input  logic                 rst_i,
     output logic                 hsync_o,
     output logic                 vsync_o,
-    output logic [X_POS_W - 1:0] x_pos_o,
-    output logic [Y_POS_W - 1:0] y_pos_o,
+    output logic [X_POS_W - 1:0] pixel_x_o,
+    output logic [Y_POS_W - 1:0] pixel_y_o,
     output logic                 visible_range_o
 );
 
@@ -72,7 +72,7 @@ module vga
     always_ff @(posedge clk_i)
         if (rst_i)
             v_cnt <= '0;
-        else if (pixel_clk_en & h_cnt_max) begin
+        else if (pixel_clk_en && h_cnt_max) begin
             v_cnt <= v_cnt + 1'b1;
 
             if (v_cnt == (V_TOTAL - 1))
@@ -84,18 +84,18 @@ module vga
         if (rst_i) begin
             hsync_o         <= '0;
             vsync_o         <= '0;
-            x_pos_o         <= '0;
-            y_pos_o         <= '0;
+            pixel_x_o       <= '0;
+            pixel_y_o       <= '0;
             visible_range_o <= '0;
 
         end else if (pixel_clk_en) begin
-            hsync_o         <= ~ (h_cnt >= HSYNC_START & h_cnt <= HSYNC_END);
-            vsync_o         <= ~ (v_cnt >= VSYNC_START & v_cnt <= VSYNC_END);
+            hsync_o         <= ~ (h_cnt >= HSYNC_START && h_cnt < HSYNC_END);
+            vsync_o         <= ~ (v_cnt >= VSYNC_START && v_cnt < VSYNC_END);
 
-            x_pos_o         <= X_POS_W' (h_cnt);
-            y_pos_o         <= Y_POS_W' (v_cnt);
+            pixel_x_o       <= X_POS_W' (h_cnt);
+            pixel_y_o       <= Y_POS_W' (v_cnt);
 
-            visible_range_o <= ((h_cnt < H_RES) & (v_cnt < V_RES));
+            visible_range_o <= ((h_cnt < H_RES) && (v_cnt < V_RES));
         end
 
 endmodule
