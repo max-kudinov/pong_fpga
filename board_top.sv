ea22117659acaac79dd6a6ce56c73614ea3e410a
mkudinov <m.kudinov@icloud.com>
2024-04-27 22:40:51 +0300

    add leds_o signal

--- board_top.sv
+++ board_top.sv
@@ -8,14 +8,19 @@ module board_top
     input  logic       clk_i,
     input  logic       rst_n_i,
     input  logic [1:0] keys_i,
+    output logic [1:0] leds_o,
     output logic [2:0] vga_rgb_o,
     output logic       vga_vs_o,
     output logic       vga_hs_o
 );
-    logic rst;
+    logic       rst;
+    logic [1:0] keys;
+    logic [1:0] leds;
 
-    // Basically the whole purpose of this module is to invert reset
-    assign rst = ~rst_n_i;
+    // Basically the whole purpose of this module is to invert signals
+    assign rst    = ~rst_n_i;
+    assign keys   = ~keys_i;
+    assign leds_o = ~leds;
 
     game_top
     # (
@@ -27,7 +32,8 @@ module board_top
     (
         .clk_i     ( clk_i     ),
         .rst_i     ( rst       ),
-        .keys_i    ( keys_i     ),
+        .keys_i    ( keys      ),
+        .leds_o    ( leds      ),
         .vga_rgb_o ( vga_rgb_o ),
         .vga_hs_o  ( vga_hs_o  ),
         .vga_vs_o  ( vga_vs_o  )
