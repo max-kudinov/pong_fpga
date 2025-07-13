37b105eba393d39d242f934557537bb21a5b23d1
Maxim Kudinov <max.kudinov@proton.me>
2025-07-14 01:46:15 +0300

    Add display interface macro

--- rtl/include/board_pkg.svh
+++ rtl/include/board_pkg.svh
@@ -4,22 +4,23 @@
 package board_pkg;
 
     `ifdef PRIMER20K
-        parameter KEYS_W       = 3;
-        parameter LEDS_W       = 2;
-        parameter VGA_RGB_W    = 24;
-        parameter RGB_W        = 24;
-        parameter COLOR_W      = 8;
-        parameter DISPLAY_TYPE = 1; // 0 - VGA, 1 - DVI (HDMI)
+        `define DVI
     `elsif ZEOWAA
-        parameter KEYS_W       = 3;
-        parameter LEDS_W       = 2;
-        parameter RGB_W        = 3;
-        parameter DISPLAY_TYPE = 0; // 0 - VGA, 1 - DVI (HDMI)
+        `define VGA
     `else
         initial $error("Wrong board configuration, either PRIMER20K or ZEOWAA should be defined")
     `endif
 
+    `ifdef PRIMER20K
+        parameter BOARD_CLK_MHZ = 25; // Pixel clock is used for everything
+        parameter KEYS_W        = 3;
+        parameter LEDS_W        = 2;
+    `elsif ZEOWAA
+        parameter BOARD_CLK_MHZ = 50;
+        parameter KEYS_W        = 3;
+        parameter LEDS_W        = 2;
+    `endif
 
 endpackage : board_pkg
 
-`endif 
+`endif
